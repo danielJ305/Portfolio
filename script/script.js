85f0821963671cb93b816d7e6cc09320d16f7d1d
Daniel Justiz <danieljustiz9508@gmail.com>
2019-08-13 23:41:42 -0400

    Work section.

---
+++ script/script.js
@@ -0,0 +1,19 @@
+const workSection = document.getElementById('work-section');
+const workMain = document.getElementById('work-main');
+const forwardArrow = document.getElementById('forward-arrow');
+const backArrow = document.getElementById('back-arrow');
+workSection.setAttribute('class', 'd-none');
+
+forwardArrow.addEventListener('click', () => {
+    workMain.setAttribute('class', 'd-none');
+    workSection.setAttribute('class', 'd-block w-100 mt-auto mb-auto d-flex work-section');
+    workSection.classList.remove('d-none');
+});
+
+backArrow.addEventListener('click', () => {
+    console.log('h')
+    workSection.setAttribute('class', 'd-none');
+    workMain.classList.remove('d-none');
+    workMain.setAttribute('class', 'd-block w-100 mt-auto mb-auto d-flex work-section');
+});
+
