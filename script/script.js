b84d4fa7c2c1ff5514d0b2b354ea1a62a0246958
Daniel Justiz <danieljustiz9508@gmail.com>
2019-10-15 12:13:33 -0400

    text and blink

--- script/script.js
+++ script/script.js
@@ -1,19 +1,9 @@
-// const workSection = document.getElementById('work-section');
-// const workMain = document.getElementById('work-main');
-// const forwardArrow = document.getElementById('forward-arrow');
-// const backArrow = document.getElementById('back-arrow');
-// workSection.setAttribute('class', 'd-none');
+$(document).ready(function () {
+    function blink() {
+        $('.blink').fadeOut();
+        $('.blink').fadeIn();
+    }
+    setInterval(blink, 1000);
+});
 
-// forwardArrow.addEventListener('click', () => {
-//     workMain.setAttribute('class', 'd-none');
-//     workSection.setAttribute('class', 'd-block w-100 mt-auto mb-auto d-flex work-section');
-//     workSection.classList.remove('d-none');
-// });
-
-// backArrow.addEventListener('click', () => {
-//     console.log('h')
-//     workSection.setAttribute('class', 'd-none');
-//     workMain.classList.remove('d-none');
-//     workMain.setAttribute('class', 'd-block w-100 mt-auto mb-auto d-flex work-section');
-// });
 
