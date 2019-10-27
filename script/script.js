e2591ddd7c9922d4f10c3fb7fab4bffb5ee5f867
Daniel Justiz <danieljustiz9508@gmail.com>
2019-10-27 15:38:56 -0400

    nav show / nav hide

--- script/script.js
+++ script/script.js
@@ -6,4 +6,18 @@ $(document).ready(function () {
     setInterval(blink, 1000);
 });
 
+$('#nav-hide').on('click touchstart', function () {
+    $('div#navigation').addClass('off');
+    // $('div#navigation').addClass('slide');
+    $('div#canvas').addClass('canvasbutton');
+});
+
+$('#nav-show').on('click touchstart', function () {
+    $('div#navigation').removeClass('off');
+    // $('div#navigation').removeClass('slide');
+    $('div#canvas').removeClass('canvasbutton');
+});
+
+
+
 
