e8d6d5186d24520a7f72d7b999ae3e1fa6764318
Daniel Justiz <danieljustiz9508@gmail.com>
2020-02-25 23:18:27 -0500

    ExperienceUpdate

--- script/script.js
+++ script/script.js
@@ -1,10 +1,4 @@
-$(document).ready(function () {
-    function blink() {
-        $('.blink').fadeOut();
-        $('.blink').fadeIn();
-    }
-    setInterval(blink, 1000);
-});
+/***  Navigation Hide/Show ***/
 
 $('#nav-hide').on('click touchstart', function () {
     $('div#navigation').addClass('off');
@@ -18,6 +12,92 @@ $('#nav-show').on('click touchstart', function () {
     $('div#canvas').removeClass('canvasbutton');
 });
 
+/*** _____________________ ***/
 
 
 
+
+/***  Work Toggle Section ***/
+
+
+var UIController = (function() {
+    
+    var DOMstrings = {
+        activeTitle: '#active-work-title',
+        unactiveTitle: '#unactive-work-title',
+        workToggleBtn: '#work-toggle',
+        graphicDesignSec: '#graphic-design',
+        webDevSec: '#work-showcase-container'
+    };
+
+    return {
+        getDOMstrings: function () {
+            return DOMstrings;
+        }
+    };
+
+})();
+
+var controller = (function(UICtrl) {
+
+    /* Public variables */
+    var toggler = 0;
+    var DOM;
+    /* ___________ */
+
+
+    var setupEventListeners = function() {
+        DOM = UICtrl.getDOMstrings();
+
+        $(DOM.workToggleBtn).on('click', workToggler);
+    };
+
+
+    function workToggler() {
+
+        if (toggler === 0) {
+            $(DOM.activeTitle).html('Graphic Design');
+            $(DOM.unactiveTitle).html('Web Development');
+            $(DOM.graphicDesignSec).css('display', 'block');
+            $(DOM.webDevSec).css('display', 'none');
+
+            toggler++;
+
+        } else if (toggler === 1) {
+            $(DOM.activeTitle).html('Web Development');
+            $(DOM.unactiveTitle).html('Graphic Design');
+            $(DOM.graphicDesignSec).css('display', 'none');
+            $(DOM.webDevSec).css('display', 'block');
+
+            toggler--;
+        }
+    }
+
+    return {
+        init: function() {
+            $(document).ready(function () {
+                toggler = 0;
+            });
+            setupEventListeners();
+        }
+    };
+
+})(UIController);
+
+controller.init();
+
+/*** _____________________ ***/
+
+
+
+
+/*** Blinking Text ***/
+
+$(document).ready(function () {
+    function blink() {
+        $('.blink').fadeOut();
+        $('.blink').fadeIn();
+    }
+    setInterval(blink, 1000);
+});
+/*** _____________________ ***/
